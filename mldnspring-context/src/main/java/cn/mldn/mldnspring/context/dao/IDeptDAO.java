d0a9ce6597b5a895b715f251932a99d1deeb9758
hh <13096947868@163.com>
2017-10-19 00:15:23 +0800

    第二天知识点

--- mldnspring-context/src/main/java/cn/mldn/mldnspring/context/dao/IDeptDAO.java
+++
@@ -1,7 +0,0 @@
-package cn.mldn.mldnspring.context.dao;
-
-import cn.mldn.mldnspring.context.vo.Dept;
-
-public interface IDeptDAO {
-	public boolean doCreate(Dept dept) ;
-}
--- mldnspring-context/src/main/java/cn/mldn/mldnspring/context/service/IDeptService.java
+++
@@ -1,7 +0,0 @@
-package cn.mldn.mldnspring.context.service;
-
-import cn.mldn.mldnspring.context.vo.Dept;
-
-public interface IDeptService {
-	public boolean add(Dept dept) ;
-}
